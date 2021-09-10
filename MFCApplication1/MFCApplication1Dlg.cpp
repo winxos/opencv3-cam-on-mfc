5d89077450676be7077f9ebf1a16ed83f2fd12a5
winxos <winxos@hotmail.com>
2021-09-10 23:07:05 +0800

    添加项目文件。

---
+++ MFCApplication1/MFCApplication1Dlg.cpp
@@ -0,0 +1,162 @@
+﻿
+// MFCApplication1Dlg.cpp: 实现文件
+//
+
+#include "pch.h"
+#include "framework.h"
+#include "MFCApplication1.h"
+#include "MFCApplication1Dlg.h"
+#include "afxdialogex.h"
+
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CMFCApplication1Dlg 对话框
+
+
+
+CMFCApplication1Dlg::CMFCApplication1Dlg(CWnd* pParent /*=nullptr*/)
+	: CDialogEx(IDD_MFCAPPLICATION1_DIALOG, pParent)
+{
+	m_hIcon = AfxGetApp()->LoadIcon(IDR_MAINFRAME);
+}
+
+void CMFCApplication1Dlg::DoDataExchange(CDataExchange* pDX)
+{
+	CDialogEx::DoDataExchange(pDX);
+	DDX_Control(pDX, IDOK, btntest);
+	DDX_Control(pDX, IDC_MFCBUTTON1, mtfb);
+	DDX_Control(pDX, IDC_PIC, pic);
+}
+
+BEGIN_MESSAGE_MAP(CMFCApplication1Dlg, CDialogEx)
+	ON_WM_PAINT()
+	ON_WM_QUERYDRAGICON()
+	ON_WM_CTLCOLOR()
+	ON_WM_TIMER()
+	ON_BN_CLICKED(IDOK, &CMFCApplication1Dlg::OnBnClickedOk)
+END_MESSAGE_MAP()
+
+int Mat2CImage(Mat* mat, CImage& img) 
+{
+	if (!mat || mat->empty())
+		return -1;
+	int nBPP = mat->channels() * 8;
+	img.Create(mat->cols, mat->rows, nBPP);
+	if (nBPP == 8)
+	{
+		static RGBQUAD pRGB[256];
+		for (int i = 0; i < 256; i++)
+			pRGB[i].rgbBlue = pRGB[i].rgbGreen = pRGB[i].rgbRed = i;
+		img.SetColorTable(0, 256, pRGB);
+	}
+	uchar* psrc = mat->data;
+	uchar* pdst = (uchar*)img.GetBits();
+	int imgPitch = img.GetPitch();
+	for (int y = 0; y < mat->rows; y++)
+	{
+		memcpy(pdst, psrc, mat->cols * mat->channels());//mat->step is incorrect for those images created by roi (sub-images!)
+		psrc += mat->step;
+		pdst += imgPitch;
+	}
+	return 0;
+}
+// CMFCApplication1Dlg 消息处理程序
+
+BOOL CMFCApplication1Dlg::OnInitDialog()
+{
+	CDialogEx::OnInitDialog();
+	// 设置此对话框的图标。  当应用程序主窗口不是对话框时，框架将自动
+	//  执行此操作
+	SetIcon(m_hIcon, TRUE);			// 设置大图标
+	SetIcon(m_hIcon, FALSE);		// 设置小图标
+
+	// TODO: 在此添加额外的初始化代码
+	CImage img;
+	img.LoadFromResource(AfxGetInstanceHandle(), MAKEINTRESOURCE(IDB_BITMAP4));
+	CBitmap bmpTmp;
+	bmpTmp.Attach(img.Detach());
+	m_brush.CreatePatternBrush(&bmpTmp);
+
+	mtfb.EnableWindowsTheming(FALSE);
+	mtfb.SetFaceColor(RGB(255, 128, 0));
+	mtfb.SetTextColor(RGB(0, 255, 255));
+	cap = new VideoCapture(0);
+	cap->set(CV_CAP_PROP_FRAME_WIDTH, 320);
+	cap->set(CV_CAP_PROP_FRAME_HEIGHT, 240);
+	SetTimer(1, 33, NULL);
+	return TRUE;  // 除非将焦点设置到控件，否则返回 TRUE
+}
+// 如果向对话框添加最小化按钮，则需要下面的代码
+//  来绘制该图标。  对于使用文档/视图模型的 MFC 应用程序，
+//  这将由框架自动完成。
+
+void CMFCApplication1Dlg::OnPaint()
+{
+	if (IsIconic())
+	{
+		CPaintDC dc(this); // 用于绘制的设备上下文
+
+		SendMessage(WM_ICONERASEBKGND, reinterpret_cast<WPARAM>(dc.GetSafeHdc()), 0);
+
+		// 使图标在工作区矩形中居中
+		int cxIcon = GetSystemMetrics(SM_CXICON);
+		int cyIcon = GetSystemMetrics(SM_CYICON);
+		CRect rect;
+		GetClientRect(&rect);
+		int x = (rect.Width() - cxIcon + 1) / 2;
+		int y = (rect.Height() - cyIcon + 1) / 2;
+
+		// 绘制图标
+		dc.DrawIcon(x, y, m_hIcon);
+	}
+	else
+	{
+		CDialogEx::OnPaint();
+	}
+}
+
+//当用户拖动最小化窗口时系统调用此函数取得光标
+//显示。
+HCURSOR CMFCApplication1Dlg::OnQueryDragIcon()
+{
+	return static_cast<HCURSOR>(m_hIcon);
+}
+
+
+
+HBRUSH CMFCApplication1Dlg::OnCtlColor(CDC* pDC, CWnd* pWnd, UINT nCtlColor)
+{
+	HBRUSH hbr = CDialogEx::OnCtlColor(pDC, pWnd, nCtlColor);
+
+	// TODO:  在此更改 DC 的任何特性
+
+	// TODO:  如果默认的不是所需画笔，则返回另一个画笔
+	return m_brush;
+}
+
+
+void CMFCApplication1Dlg::OnTimer(UINT_PTR nIDEvent)
+{
+	// TODO: 在此添加消息处理程序代码和/或调用默认值
+	static int i = 0;
+	CString str;
+	str.Format(_T("%d"), i++);
+	mtfb.SetWindowTextW(str);
+	Mat frame;
+	*cap >> frame;
+	CImage img;
+	Mat2CImage(&frame, img);
+	pic.SetBitmap(HBITMAP(img));
+	CDialogEx::OnTimer(nIDEvent);
+}
+
+
+void CMFCApplication1Dlg::OnBnClickedOk()
+{
+	// TODO: 在此添加控件通知处理程序代码
+	CDialogEx::OnOK();
+}
---
+++ MFCApplication1/resource.h
@@ -0,0 +1,23 @@
+﻿//{{NO_DEPENDENCIES}}
+// Microsoft Visual C++ 生成的包含文件。
+// 供 MFCApplication1.rc 使用
+//
+#define IDD_MFCAPPLICATION1_DIALOG      102
+#define IDR_MAINFRAME                   128
+#define IDB_BITMAP1                     130
+#define IDB_BITMAP2                     131
+#define IDB_BITMAP3                     132
+#define IDB_BITMAP4                     134
+#define IDC_MFCBUTTON1                  1000
+#define IDC_PIC                         1003
+
+// Next default values for new objects
+// 
+#ifdef APSTUDIO_INVOKED
+#ifndef APSTUDIO_READONLY_SYMBOLS
+#define _APS_NEXT_RESOURCE_VALUE        135
+#define _APS_NEXT_COMMAND_VALUE         32771
+#define _APS_NEXT_CONTROL_VALUE         1004
+#define _APS_NEXT_SYMED_VALUE           101
+#endif
+#endif
